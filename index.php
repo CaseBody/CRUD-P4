3156a42b7b7db9c326e0ce6d44cf29855d6c90e6
RickRietbergen <rbps3.2016@gmail.com>
2022-05-11 14:15:06 +0200

    header completed

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>startpagina</title>
     <!-- link css -->
     <link rel="stylesheet" href="CSS/styles.css" />
     <!-- icon page -->
     <link rel="icon" type="image/x-icon" href="Assets/afbeeldingen/.png" />
     <!-- fonts -->
+
   </head>
   <body>
     <main>
@@ -17,10 +18,22 @@
         <nav>
           <li>
             <ul>
-              <a href="#">test</a>
+              <a href="#" class="txt_logo">
+                <img class="img_logo" src="Assets/afbeeldingen/logo.png" alt="logo website">
+                <p>TRAVELEGY</p>
+              </a>
             </ul>
-            <ul>
-                <a href="#">test    </a>
+            <ul id="txt_header" class="ul_header">  
+              <a href="#">start</a>
+            </ul>
+            <ul class="ul_header">
+                <a href="#">aanbevolen</a>
+            </ul>
+            <ul class="ul_header">
+                <a href="#">destinations</a>
+            </ul>
+            <ul class="ul_header">
+                <a href="#">contact</a>
             </ul>
           </li>
         </nav>
