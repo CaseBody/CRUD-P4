a3169e902d584406345730919024120487952dec
RickRietbergen <rbps3.2016@gmail.com>
2022-05-11 13:14:31 +0200

    header

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <!-- link css -->
+    <link rel="stylesheet" href="CSS/styles.css" />
+    <!-- icon page -->
+    <link rel="icon" type="image/x-icon" href="Assets/afbeeldingen/.png" />
+    <!-- fonts -->
+  </head>
+  <body>
+    <main>
+      <header>
+        <nav>
+          <li>
+            <ul>
+              <a href="#">test</a>
+            </ul>
+            <ul>
+                <a href="#">test    </a>
+            </ul>
+          </li>
+        </nav>
+      </header>
+      <div class="container_startpage"></div>
+
+      <div class="container_aanbevolen_reizen"></div>
+    </main>
+  </body>
+</html>
