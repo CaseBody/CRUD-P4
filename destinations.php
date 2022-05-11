8cb7d5f60b55e2a51697ab61390ad45d38e4873b
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-05-11 14:23:11 +0200

    boven gedeelte

---
+++ destinations.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Destinations</title>
+		<link rel="stylesheet" href="CSS/styles.css" />
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+	</head>
+	<body class="destinations_pagina">
+		<div class="content">
+			<div class="top">
+				<p class="destinations_title">Discover the Travelegy destinations</p>
+				<div class="search_bar">
+					<input type="text" placeholder="Search by name..." />
+					<button>Search</button>
+				</div>
+			</div>
+
+			<div class="categories">
+				<div class="categorie">
+					<p class="categorie_titel">Categorie naam</p>
+					<div class="item">
+						<p class="item_titel">Reis naam</p>
+						<p class="item_subtitel">Boek nu <img src="Assets/afbeeldingen/arrow_right.png" /></p>
+					</div>
+
+					<p class="categorie_titel">Categorie naam</p>
+					<div class="item">
+						<p class="item_titel">Reis naam</p>
+						<p class="item_subtitel">Boek nu <img src="Assets/afbeeldingen/arrow_right.png" /></p>
+					</div>
+
+					<p class="categorie_titel">Categorie naam</p>
+					<div class="item">
+						<p class="item_titel">Reis naam</p>
+						<p class="item_subtitel">Boek nu <img src="Assets/afbeeldingen/arrow_right.png" /></p>
+					</div>
+
+					<p class="categorie_titel">Categorie naam</p>
+					<div class="item">
+						<p class="item_titel">Reis naam</p>
+						<p class="item_subtitel">Boek nu <img src="Assets/afbeeldingen/arrow_right.png" /></p>
+					</div>
+
+					<p class="categorie_titel">Categorie naam</p>
+					<div class="item">
+						<p class="item_titel">Reis naam</p>
+						<p class="item_subtitel">Boek nu <img src="Assets/afbeeldingen/arrow_right.png" /></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
