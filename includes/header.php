36e2c3cddc0f1b7d1ee9d9a2f9d55a24c4d22e0f
RickRietbergen <rbps3.2016@gmail.com>
2022-05-11 14:22:10 +0200

    header + php

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,24 @@
 <header>
     <nav>
         <li>
-            <ul>
-                <a href="#"></a>
-            </ul>
+        <ul>
+            <a href="#" class="txt_logo">
+            <img class="img_logo" src="Assets/afbeeldingen/logo.png" alt="logo website">
+            <p>TRAVELEGY</p>
+            </a>
+        </ul>
+        <ul id="txt_header" class="ul_header">  
+            <a href="#">start</a>
+        </ul>
+        <ul class="ul_header">
+            <a href="#">aanbevolen</a>
+        </ul>
+        <ul class="ul_header">
+            <a href="#">destinations</a>
+        </ul>
+        <ul class="ul_header">
+            <a href="#">contact</a>
+        </ul>
         </li>
     </nav>
 </header>
--- index.php
+++ index.php
@@ -12,32 +12,12 @@
     <!-- fonts -->
 
   </head>
+
+  <?php
+    require_once 'includes/header.php';
+  ?>
   <body>
     <main>
-      <header>
-        <nav>
-          <li>
-            <ul>
-              <a href="#" class="txt_logo">
-                <img class="img_logo" src="Assets/afbeeldingen/logo.png" alt="logo website">
-                <p>TRAVELEGY</p>
-              </a>
-            </ul>
-            <ul id="txt_header" class="ul_header">  
-              <a href="#">start</a>
-            </ul>
-            <ul class="ul_header">
-                <a href="#">aanbevolen</a>
-            </ul>
-            <ul class="ul_header">
-                <a href="#">destinations</a>
-            </ul>
-            <ul class="ul_header">
-                <a href="#">contact</a>
-            </ul>
-          </li>
-        </nav>
-      </header>
       <div class="container_startpage"></div>
 
       <div class="container_aanbevolen_reizen"></div>
