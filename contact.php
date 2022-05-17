c0554222e5c3998f99557a1f9a5b7c3b6cf7ae82
RickRietbergen <rbps3.2016@gmail.com>
2022-05-17 13:54:01 +0200

    first things

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>contactpagina</title>
+  <!-- link css -->
+  <link rel="stylesheet" href="CSS/styles.css"/>
+  <!-- icon page -->
+    <link rel="icon" type="image/x-icon" href="Assets/afbeeldingen/.png" />
+    <!-- fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
+  <link rel="preconnect" href="https://fonts.gstatic.com/"crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+
+  <?php
+    require_once "includes/header.php";
+  ?>
+
+<body>
+  <main>
+  <div class="container_contact">
+    <form action="">
+      <label for="name">Name</label>
+      <input type="text" placeholder="you're name">
+    </form>
+  </div>
+  </main>
+</body>
+</html>
