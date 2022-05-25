19c5175b784fdb0df9a6a685a08d40d2dca82eea
RickRietbergen <rbps3.2016@gmail.com>
2022-05-25 14:39:11 +0200

    account page start, icon to tab added

---
+++ account.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    <!-- link css -->
+    <link rel="stylesheet" href="CSS/styles.css" />
+    <!-- icon page -->
+    <link rel="icon" type="image/x-icon" href="Assets/afbeeldingen/logo.png"/>
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+    <?php
+        require_once 'includes/header.php';
+    ?>
+<body>
+    <main>
+        <div class="container_account">
+            <div class="left">
+                <div class="nav_title">
+                    <p>My Account</p>
+                </div>
+
+                <div class="nav">
+                    <div class="link">
+                        <img src="" alt="">
+                        <p id="">test</p>
+                    </div>
+                    <div class="link">
+                        <img src="" alt="">
+                        <p id="">test</p>
+                    </div>
+                    <div class="link">
+                        <img src="" alt="">
+                        <p id="">test</p>
+                    </div>
+                    <div class="link">
+                        <img src="" alt="">
+                        <p id="">test</p>
+                    </div>
+                    <div class="link">
+                        <img src="" alt="">
+                        <p id="">test</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="right">
+                <div class="acc_title">
+                    <p>Welcome username</p>
+                </div>
+                <div class="acc_info">
+                    <div class="personal_info">
+                        <div class="personal_title">
+                            <p>Personal Information</p>
+                        </div>
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
--- create_account.php
+++ create_account.php
@@ -5,7 +5,10 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Login</title>
+		<!-- link css -->
 		<link rel="stylesheet" href="CSS/styles.css" />
+		<!-- icon page -->
+	    <link rel="icon" type="image/x-icon" href="Assets/afbeeldingen/logo.png"/>
 		<!-- fonts -->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
