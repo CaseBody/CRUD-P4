ae0f754acf650a1b17e9011be47d81118a390968
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-06-01 14:40:32 +0200

    boven gedeelte af

---
+++ reis.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Reis</title>
+		<link rel="stylesheet" href="CSS/styles.css" />
+		<!-- fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
+	</head>
+	<body class="reis_pagina">
+		<?php 
+		include_once "includes/header.php";
+	?>
+		<main>
+			<div class="main_content">
+				<div class="left">
+					<img src="https://images.corendonresources.com/L1E10009A1W600H480.jpg?v=220522084610" alt="" />
+				</div>
+				<div class="right">
+					<p class="title">The Three Corners Resort</p>
+					<p class="sub_title">Cairo, Egypt</p>
+					<p class="rating">★★★★★</p>
+					<div class="prijs-button">
+						<p class="prijs">Nu vanaf <span>€ 90.00</span></p>
+						<button>Boek Nu</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="tabs">
+				<ul>
+					<li>Recensies</li>
+					<li>Beschrijving</li>
+					<li>Boek nu</li>
+				</ul>
+			</div>
+
+			<div class="secondary_content"></div>
+		</main>
+	</body>
+</html>
