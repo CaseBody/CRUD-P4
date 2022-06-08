6e0da556524372e87d012e17e626ea7d6d20ad58
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-06-08 15:01:45 +0200

    tabs

--- reis.php
+++ reis.php
@@ -23,20 +23,10 @@
 			}
 
 			$sql = 'SELECT * FROM reis WHERE reis.id = :id';
-			$stmt = $connect->prepare($sql);
-			$stmt->bindParam('id', $_GET['id']);
-			$stmt->execute();
-			$result = $stmt->fetchAll();
-			$result = $result[0];
-
-			$sql = 'SELECT * FROM recensies  
-			INNER JOIN gebruikers ON recensies.gebruikerid=gebruikers.id 
-			WHERE reisid = :id';
-			$stmt = $connect->prepare($sql);
-			$stmt->bindParam('id', $_GET['id']);
-			$stmt->execute();
-			$result_recensies = $stmt->fetchAll();
-		?>
+			$stmt = $connect->prepare($sql); $stmt->bindParam('id', $_GET['id']); $stmt->execute(); $result = $stmt->fetchAll(); $result
+		= $result[0]; $sql = 'SELECT * FROM recensies INNER JOIN gebruikers ON recensies.gebruikerid=gebruikers.id WHERE reisid =
+		:id'; $stmt = $connect->prepare($sql); $stmt->bindParam('id', $_GET['id']); $stmt->execute(); $result_recensies =
+		$stmt->fetchAll(); ?>
 		<main>
 			<div class="main_content">
 				<div class="left">
@@ -45,54 +35,57 @@
 				<div class="right">
 					<p class="title"><?php echo $result['naam'] ?></p>
 					<p class="sub_title"><?php echo $result['locatie'] ?></p>
-					<p class="rating"><?php
-						if (count($result_recensies) > 0)
-						{
-							$total = 0;
-
-							foreach ($result_recensies as $recensie)
-							{
-								$total += (int)$recensie['sterren'];
-							}
-
-							$total = $total / count($result_recensies);
-
-							for ($i = 0; $i < $total; $i++)
-							{
-								echo "★";
-							} 
-						}
-					?></p>
+					<p class="rating">
+						<?php
+						if (count($result_recensies) >
+						0) { $total = 0; foreach ($result_recensies as $recensie) { $total += (int)$recensie['sterren']; } $total =
+						$total / count($result_recensies); for ($i = 0; $i < $total; $i++) { echo "★"; } } ?>
+					</p>
 					<div class="prijs-button">
-						<p class="prijs">Nu vanaf <span>€ <?php echo $result['prijs'] ?></span></p>
-						<button>Boek Nu</button>
+						<p class="prijs">
+							Now from
+							<span
+								>€
+								<?php echo $result['prijs'] ?></span
+							>
+						</p>
+						<button>Book Now</button>
 					</div>
 				</div>
 			</div>
 
 			<div class="tabs">
 				<ul>
-					<li class="selected">Recensies</li>
-					<li>Beschrijving</li>
-					<li>Boek nu</li>
+					<li id="beschrijving_button" class="selected">Description</li>
+					<li id="recensies_button">Reviews</li>
+					<li id="boek_button">Book Now</li>
 				</ul>
 
-				<div class="recensies">
+				<div id="beschrijving" class="main_beschrijving">
+					<?php echo $result['beschrijving'] ?>					
+				</div>
 
+				<div id="recensies" class="recensies">
 					<?php 
 
 						foreach ($result_recensies as $recensie) {
 							?>
-							<div class="recensie">
-								<p class="gebruiker_sterren"><?php echo $recensie['voornaam'] ?> - <span><?php
+					<div class="recensie">
+						<p class="gebruiker_sterren">
+							<?php echo $recensie['voornaam'] ?>
+							-
+							<span
+								><?php
 								for ($i = 0; $i < $recensie['sterren']; $i++)
 								{
 									echo "★";
 								} 
-								?></span></p>
-								<p class="beschrijving"><?php echo $recensie['beschrijving'] ?></p>
-							</div>
-							<?php
+								?></span
+							>
+						</p>
+						<p class="beschrijving"><?php echo $recensie['beschrijving'] ?></p>
+					</div>
+					<?php
 						}
 
 					?>
@@ -102,4 +95,6 @@
 			<div class="secondary_content"></div>
 		</main>
 	</body>
+
+	<script src="js/reis.js"></script>
 </html>
